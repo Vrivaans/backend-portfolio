a20bb2e9048316f95be3f4b70a60c1de2ac58b69
Ivan Vidal <vrivaans@gmail.com>
2022-10-31 18:44:22 -0300

    modificacion cross origin

--- src/main/java/com/portafolio/backend/Controllers/AppRestController.java
+++ src/main/java/com/portafolio/backend/Controllers/AppRestController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // @CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://vrivaansportfolio.web.app/")
+@CrossOrigin(origins = { "https://vrivaansportfolio.web.app/", "http://localhost:4200" })
 public class AppRestController {
 
     @Autowired
