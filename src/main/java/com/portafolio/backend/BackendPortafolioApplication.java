7f1847e2d868d9668502660eb96b7141a5df4185
Ivan Vidal <vrivaans@gmail.com>
2022-11-01 10:09:00 -0300

    prueba con gloud sql

--- src/main/java/com/portafolio/backend/BackendPortafolioApplication.java
+++ src/main/java/com/portafolio/backend/BackendPortafolioApplication.java
@@ -1,9 +1,11 @@
 package com.portafolio.backend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class BackendPortafolioApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/portafolio/backend/Controllers/AppRestController.java
+++ src/main/java/com/portafolio/backend/Controllers/AppRestController.java
@@ -24,8 +24,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-// @CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = { "https://vrivaansportfolio.web.app/", "http://localhost:4200" })
+
+@CrossOrigin(origins = { "https://vrivaansportfolio.web.app/", "localhost:4200" })
 public class AppRestController {
 
     @Autowired
