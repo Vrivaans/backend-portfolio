e7ad5154c8d1219626d57353f7c017c680a84a51
Vrivaans <vrivaanss@gmail.com>
2023-01-19 14:35:48 -0300

    probe cambiando spingrunApplication en la main

---
+++ BackendPortafolioApplication.java
@@ -0,0 +1,15 @@
+package com.portafolio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class BackendPortafolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPortafolioApplication.java, args);
+	}
+
+}
